00f8784cb8e698d857b64e895bcb5412bc211683
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2024-03-13 01:19:10 +0530

    created navbar

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <nav className="nav-container">
+        <div className="logo-container">
+            
+        </div>
+        <div className="header-links">
+          <ul>
+            <li>
+
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
